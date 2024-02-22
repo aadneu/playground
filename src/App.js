c0d8e134ff464ca9ee11b42d8a374883ae8e06f1
aadneu <141396377+aadneu@users.noreply.github.com>
2024-02-22 18:54:40 +0100

    hashrouter

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import './comps/Games/Game2/game2.css'
 import './comps/Games/Snake/Snake.css'
 
 import Home from './comps/Home'
-import { BrowserRouter as Router} from 'react-router-dom'
 
 function App() {
   return (
 
     <div className="App">
-      <Router>
+      
         <Home/> 
-      </Router>
+      
     </div>
 
 
