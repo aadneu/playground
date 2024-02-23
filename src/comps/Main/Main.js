6a1faa79d5e211ca077a62e582d0403e8e1e4ff4
aadneu <aadneu@gmail.com>
2024-02-23 09:15:51 +0100

    Update Main.js

--- src/comps/Main/Main.js
+++ src/comps/Main/Main.js
@@ -3,7 +3,8 @@ import React from 'react'
 const Main = () => {
   return (
     <div>
-        <h2>MAIN PAGE</h2>
+        <h2>VELKOMMEN</h2>
+        <p>Jeg vil forsøke å oppdatere denne siden fortløpende etter hver som jeg lærer meg nye ting </p>
     </div>
   )
 }
