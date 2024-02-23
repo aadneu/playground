ce807b27d06e00c9c397bdc8fb6d5370220e79eb
aadneu <aadneu@gmail.com>
2024-02-23 08:19:25 +0100

    x

--- src/App.js
+++ src/App.js
@@ -8,10 +8,8 @@ function App() {
   return (
 
     <div className="App">
-      
-        <Home/> 
-      
-    </div>
+       <Home/> 
+   </div>
 
 
     
--- src/index.js
+++ src/index.js
@@ -9,15 +9,12 @@ import { HashRouter as Router} from 'react-router-dom'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  // <React.StrictMode> for at det ikke skal rendre 2x
-<Router>
-   <App />
-</Router>
+  <Router>
+    <App />
+  </Router>
    
 
   //</React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
